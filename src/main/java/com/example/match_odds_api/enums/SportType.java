33eb359c8d6c0e8663c4f032fdea0a8660d8c309
PanosZacharatos <panoszah@gmail.com>
2025-06-19 13:23:11 +0300

    Dockerize the app

--- src/main/java/com/example/match_odds_api/enums/SportType.java
+++ src/main/java/com/example/match_odds_api/enums/SportType.java
@@ -1,5 +1,6 @@
 package com.example.match_odds_api.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum SportType {
@@ -15,5 +16,15 @@ public enum SportType {
 
     public Integer getValue(){
         return value;
+    }
+
+    @JsonCreator
+    public static SportType valueFromSport(Integer value){
+        for(SportType type: SportType.values()){
+            if(type.value == value){
+                return type;
+            }
+        }
+        throw new IllegalArgumentException(value +" does not exist");
     }
 }
